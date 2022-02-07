9e6a21b33d3be54df23c210b72311f274dbcd94f
Ricardo Wu <wuhun@oregonstate.edu>
2022-02-06 19:09:48 -0800

    Finally got it fixed, my token for /v1/weather and /v1/hello

--- server.js
+++ server.js
@@ -2,18 +2,12 @@
 
 var express = require('express')
 const bodyParser = require("body-parser");
-var path = require('path')
+var path = require('path');
+const { sendStatus } = require('express/lib/response');
 var app = express()
-
-//JWT:
-const jwt = require('jsonwebtoken');
-const { request } = require('http');
-const { response } = require('express');
-app.use(express.json())
 
 app.listen(3000)
 console.log('Node.js Express server is running on part 3000...')
-
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
@@ -39,22 +33,51 @@ app.use(function (req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//verify token, adding variables:
-app.get('/v1/weather', token_authentication, (req,res))
+// Verify token, adding variables:
+app.get('/v1/weather', get_weather)
+
+
 function get_weather(request, response)
 {
-    response.json
-    (
-        {"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"base":"stations","main":{"temp":278.05,"feels_like":278.05,"temp_min":276.45,"temp_max":279.76,"pressure":1032,"humidity":66},"visibility":10000,"wind":{"speed":0.45,"deg":113,"gust":0.89},"clouds":{"all":59},"dt":1642789417,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642779731,"sunset":1642813576},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200}
-    )
+    const bearHeader = request.headers['authorization'];
+    console.log(bearHeader);
+    // const bearer = bearHeader.split(' ')[1];
+    console.log(bearHeader.split('  '))
+    const bearerToken = bearHeader.split(' ')[0]
+    // console.log('this is my bearer:', bearer);
+    console.log('my bearerToken: ', bearerToken);
+    if (is_valid_token(bearerToken)) {
+        response.json(
+            { "coord": { "lon": -123.262, "lat": 44.5646 }, "weather": [{ "id": 803, "main": "Clouds", "description": "broken clouds", "icon": "04d" }], "base": "stations", "main": { "temp": 278.05, "feels_like": 278.05, "temp_min": 276.45, "temp_max": 279.76, "pressure": 1032, "humidity": 66 }, "visibility": 10000, "wind": { "speed": 0.45, "deg": 113, "gust": 0.89 }, "clouds": { "all": 59 }, "dt": 1642789417, "sys": { "type": 2, "id": 2040223, "country": "US", "sunrise": 1642779731, "sunset": 1642813576 }, "timezone": -28800, "id": 5720727, "name": "Corvallis", "cod": 200 })
+
+    } else {
+        return response.sendStatus(401);
+    }
+    
 }
-//adding variables just like /v1/weather
-app.get('/v1/hello', token_authentication, (req,res))
-function hello(request, response){
-    response.json
-    ( 
-        {"message": "Hello from Ricardo"} 
-    )
+
+// Adding variables just like /v1/weather
+app.get('/v1/hello', hello)
+
+// If the request is correct, response with message:
+function hello(request, response) {
+    const bearHeader = request.headers['authorization'];
+    console.log(bearHeader);
+    // const bearer = bearHeader.split(' ')[1];
+    console.log(bearHeader.split('  '))
+    const bearerToken = bearHeader.split(' ')[0]
+    // console.log('this is my bearer:', bearer);
+    console.log('my bearerToken: ', bearerToken);
+    if (is_valid_token(bearerToken)) {
+        response.json
+        ( 
+            {"message": "Hello from Ricardo"} 
+        )
+    }
+    else{
+        return response.sendStatus(401);
+    }
+   
 }
 
 app.post('/v1/auth', authorization)
@@ -69,16 +92,17 @@ function authorization(request, response)
         "expire": "2022-02-24T14:14:30:000Z"
     })
 }
-//a function to verify mock token here:
-function token_authentication(request, response, next){
-    const authHeader = request.headers['authorization']
-    const token = authHeader && authHeader.spilt(' ')[1]
 
-    if(token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUmljYXJkbyBXdSIsImV4cGlyZWQgZGF0ZSI6IkZlYi4gMjR0aCwgMjAyMi4ifQ.n_3i2eW-C8CRNOpdx5qkzBIMUb7O_Z4EPPZmDL-Jlgk"){
-        next();
+// A function to verify mock token here:
+function is_valid_token(bearerToken) {
+   
+    if (bearerToken === "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiUmljYXJkbyBXdSIsImV4cGlyZWQgZGF0ZSI6IkZlYi4gMjR0aCwgMjAyMi4ifQ.n_3i2eW-C8CRNOpdx5qkzBIMUb7O_Z4EPPZmDL-Jlgk"){
+        console.log("correct");
+        return true
     }
     else{
-        return sendStatus(401);
+        console.log("incorrect");
+        return false
     }
 }
 
